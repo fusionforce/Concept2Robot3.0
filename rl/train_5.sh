fa5498e50352ed22f6d03d9ddda090e0f32d229c
Sai Shruthi Balaji <shruthibalaji2307@gmail.com>
2023-03-29 03:09:00 +0000

    training config

--- rl/train_5.sh
+++ rl/train_5.sh
@@ -1,9 +1,12 @@
 python3 run.py\
         --task_id 5\
-        --exp_name "without_force_goal_only"\
+        --exp_name "text-only"\
         --gui \
+        --force_term \
         --stage train\
         --batch_size 64\
         --action_penalty 0.2\
-        --start_learning_episode 1000\
-        --comment "action_penalty_0.2_critic_goal_only"
+        --start_learning_episode 500\
+        --saving_model_freq 500\
+        --max_ep 5005 \
+        --comment "text-only model"\
